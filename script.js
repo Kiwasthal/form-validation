8702ae6c75bf89d00c56a87812ba394cde6b34a5
Steerpike <imthekoul@gmail.com>
2022-05-04 03:49:46 +0300

    Added zip-code validation

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const countryInput = document.querySelector('#country');
 const errorCountry = document.querySelector('.errorCountry');
 const zipInput = document.querySelector('#zipcode');
+const errorZipcode = document.querySelector('.errorZipcode');
 const passwordInput = document.querySelector('#password');
 const confirmPasswordInput = document.querySelector('#confirmPassword');
 const emailInput = document.querySelector('#email');
+const regZip = new RegExp('^[0-9]*$');
 
 countryInput.addEventListener('input', () => {
   countryInput.addEventListener('mouseleave', () => {
@@ -17,6 +19,27 @@ countryInput.addEventListener('click', () => {
     if (countryInput.validity.valueMissing) {
       countryInput.style.backgroundColor = '#ffb5b5';
       errorCountry.textContent = '*This field can not be empty';
+    }
+  });
+});
+
+zipInput.addEventListener('input', () => {
+  errorZipcode.textContent = '';
+  zipInput.style.backgroundColor = '#ffffff';
+});
+
+zipInput.addEventListener('click', () => {
+  zipInput.addEventListener('mouseleave', () => {
+    if (zipInput.validity.valueMissing) {
+      zipInput.style.backgroundColor = '#ffb5b5';
+      errorZipcode.textContent = '*This field can not be empty';
+    } else if (zipInput.validity.tooShort) {
+      zipInput.style.backgroundColor = '#ffb5b5';
+      errorZipcode.textContent =
+        '*This field can not be less than 4 digits long';
+    } else if (regZip.test(zipInput.value) == false) {
+      zipInput.style.backgroundColor = '#ffb5b5';
+      errorZipcode.textContent = '*This field should contain numbers';
     }
   });
 });
